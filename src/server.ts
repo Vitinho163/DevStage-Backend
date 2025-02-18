9e4a45953b6b426aacfcc34bf4fd976f7fdb1ffe
João Victor <victor.jvsb18@gmail.com>
2025-02-17 22:05:06 -0300

    add environment configuration, API route for subscriptions, and Swagger documentation

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
-import { fastify } from 'fastify'
 import { fastifyCors } from '@fastify/cors'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
+import { fastify } from 'fastify'
 import {
-  validatorCompiler,
+  type ZodTypeProvider,
+  jsonSchemaTransform,
   serializerCompiler,
-  ZodTypeProvider,
+  validatorCompiler,
 } from 'fastify-type-provider-zod'
-import { z } from 'zod'
+import { env } from './env'
+import { subscribeToEventRoute } from './routes/subscribe-to-event-route'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -14,22 +18,22 @@ app.setValidatorCompiler(validatorCompiler)
 
 app.register(fastifyCors)
 
-app.post('/subscriptions', {
-  schema: {
-    body: z.object({
-      name: z.string(),
-      email: z.string().email(),
-    })
-  }
-}, (request, reply) => {
-  const { name, email } = request.body
-
-  return reply.status(201).send({
-    name,
-    email,
-  })
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'NLW Connect',
+      version: '0.0.1',
+    },
+  },
+  transform: jsonSchemaTransform,
 })
 
-app.listen({ port: 3333 }).then(() => {
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+app.register(subscribeToEventRoute)
+
+app.listen({ port: env.PORT }).then(() => {
   console.log('HTTP server running')
 })
