8b174f8c84593f896fd8d23756a1439a456711e4
João Victor <victor.jvsb18@gmail.com>
2025-02-28 01:27:25 -0300

    add OpenAI tools for enhanced user message responses and update example environment file

--- src/functions/answer-user-message.ts
+++ src/functions/answer-user-message.ts
@@ -1,5 +1,9 @@
-import { generateText } from "ai"
-import { openai } from "../ai/openai"
+import { generateText, tool } from 'ai'
+import z from 'zod'
+import { openai } from '../ai/openai'
+import { postgresTool } from '../ai/tools/postgres-tool'
+import { pg } from '../drizzle/client'
+import { redisTool } from '../ai/tools/redis-tool'
 
 interface AnswerUserMessageParams {
   message: string
@@ -9,7 +13,18 @@ export async function answerUserMessage({ message }: AnswerUserMessageParams) {
   const answer = await generateText({
     model: openai,
     prompt: message,
-    system: 'Você é uma I.A que não sabe responder nada, responda toda pergunta com "não sei".',
+    tools: {
+      postgresTool,
+      redisTool,
+    },
+    system: `
+      Você é um assistente de I.A, responsável por responder dúvidas sobre um evento de programação.
+    
+      Inclua na resposta somente o que o usuário pediu, sem nenhum texto adicional.
+    
+      O retorno deve ser sempre em markdown (sem incluir \`\`\` no inicio ou no fim).
+    `.trim(),
+    maxSteps: 5,
   })
 
   return { response: answer.text }
